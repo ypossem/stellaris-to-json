6974743748d752cd396c0190edb8441ee99c766d
York Possemiers <github@yorkp.name>
2024-09-04 22:07:30 +0930

    Object.filter doesn't seem standard

--- src/tech.ts
+++ src/tech.ts
@@ -1,11 +1,52 @@
 import { readFileSync } from "fs";
+import { glob } from "glob";
 import path from "path";
 import * as peggy from "peggy";
+import {mkdir, readFile} from 'node:fs/promises'
 
+const OUT_DIR_PATH = path.join(__dirname, "json");
 
-export function readTech(baseDir: string) {
+const readSingleTech = (baseDir: string, stellarisParser: peggy.Parser)=>
+    async (techDataFilePath: string) => {
+    if(
+        ["scripted_variables", "random_names"]
+        .some(v => techDataFilePath.includes("/" + v + "/"))
+        || ["_tags", "README", "HOW_TO_MAKE_NEW_SHIPS"]
+        .some(v => techDataFilePath.endsWith(v + ".txt"))
+    ) {
+        return
+    }
+    const outFileDirPath = path.join(OUT_DIR_PATH, path.dirname(path.relative(baseDir, techDataFilePath)));
+    await mkdir(outFileDirPath, {recursive: true})
+    let techDataRaw = await readFile(techDataFilePath, {encoding: 'utf-8'})
+    // Clean up some typos in some of the files so the PEGJS parser doesn't choke
+    const knownIssueFixes = [
+        [/min = ([0-9]+) max ([0-9]+)/g, "min = $1 max = $2"]	// Missing equal sign in some case
+    ]
+    knownIssueFixes.forEach(v => { techDataRaw = techDataRaw.replace(v[0], v[1]); });
+
+    const techData = stellarisParser.parse(techDataRaw);
+    
+    // Extract our our local vars
+    const localVars = {};
+    
+    Object.filter(techData, (k, v) =>
+    {
+        if(!k.startsWith("@"))
+            return true;
+
+        localVars[k] = v;
+        return false;
+    });
+}
+
+export async function readTech(baseDir: string) {
     const stellarisGrammarPath = path.join(__dirname, "stellaris.pegjs")
     const stellarisGrammarRaw = readFileSync(stellarisGrammarPath, {encoding: 'utf8'})
 
-    const stellarisParser = peggy.generate(stellarisGrammarRaw) 
+    const stellarisParser = peggy.generate(stellarisGrammarRaw)
+    const files = glob(path.join(baseDir, "**/*.txt"))
+
+    await Promise.allSettled((await files).map(readSingleTech)
+
 }
